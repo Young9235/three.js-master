496775686ed1d97d7a1c23c2354eaf2318faaa4d
IN YOUNG <inyoung9235@daum.net>
2022-06-24 18:09:03 +0900

    2022-06-24 커밋

---
+++ unitTest/gltf_exam.js
@@ -0,0 +1,99 @@
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
+import { GLTFLoader } from "../examples/jsm/loaders/GLTFLoader.js"
+
+class App {
+    constructor() {
+        const divContainer = document.querySelector("#webgl-container");
+        this._divContainer = divContainer;
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        divContainer.appendChild(renderer.domElement);
+
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        this._scene = scene;
+
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setupControls() {
+        new OrbitControls(this._camera, this._divContainer);
+    }
+
+    _setupModel() {
+        const gltfLoader = new GLTFLoader();
+        const url = 'data/bridge/scene.gltf'
+        gltfLoader.load(
+            url,
+            (gltf) => {
+                const root = gltf.scene;
+                this._scene.add(root);
+            }
+        );
+    }
+
+    _zoomFit(object3D, camera) {
+        // 3차원 모델의 경계 박스
+        const box = new THREE.Box3().setFromObject(object3D);
+
+        // 모델의 경계 박스 대각 길이
+        const sizeBox = box.getSize(new THREE.Ve);
+    }
+
+    _setupCamera() {
+        const camera = new THREE.PerspectiveCamera(
+            75, 
+            window.innerWidth / window.innerHeight, 
+            0.1, 
+            100
+        );
+
+        camera.position.z = 30;
+        this._camera = camera;
+        this._scene.add(camera);
+    }
+
+    _setupLight() {
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        this._camera.add(light);
+    }
+
+    update(time) {
+        time *= 0.001; // second unit
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);   
+        this.update(time);
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    resize() {
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+        
+        this._renderer.setSize(width, height);
+    }
+}
+
+window.onload = function () {
+    new App();
+}
